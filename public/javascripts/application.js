1f5b584ca96a75646485a4e0712f3dd9b45e97af
Mykolas Viningas <m.viningas@gmail.com>
2022-09-26 21:25:25 +0100

    Added the ability to create more inputs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,20 @@ $(()=> {
   $("#save_income .add_input").click(event => {
     event.preventDefault();
 
-    console.log(event.target);
+    let $input_wrapper = $("#save_income fieldset").children(".input_wrapper").last();
+    let $new_input_wrapper = $input_wrapper.clone();
+
+    $new_input_wrapper.find("label, input, select").each((_, element) => {
+      let $element = $(element);
+
+      if (element.tagName === "LABEL") {
+        $element.attr("for", $element.attr("for").replace(/\d$/, num => String(Number(num) + 1)));
+      } else {
+        $element.attr("id", $element.attr("id").replace(/\d$/, num => String(Number(num) + 1)));
+        $element.attr("name", $element.attr("name").replace(/\d$/, num => String(Number(num) + 1)));
+      }
+    });
+
+    $("#save_income fieldset").append($new_input_wrapper);
   });
 });
