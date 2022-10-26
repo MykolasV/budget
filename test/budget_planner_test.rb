28e37ff6115e623d1d2e714d10e5a427a5965217
Mykolas Viningas <m.viningas@gmail.com>
2022-10-26 23:15:21 +0100

    Added tests for expenses page

--- test/budget_planner_test.rb
+++ test/budget_planner_test.rb
@@ -20,4 +20,27 @@ class BudgetTest < Minitest::Test
     assert_includes(last_response.body, '<a herf="#" class="add_input">+ Income</a>')
     assert_includes(last_response.body, '<button type="submit">Save</button>')
   end
+
+  def test_expenses_with_no_income
+    get "/expenses"
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, '<p>Please provide some details about your income.</p>')
+    assert_includes(last_response.body, '<form method="post" action="/income" id="save_income">')
+  end
+
+  def test_expenses
+    post "/income", { income_name_1: "salary", income_amount_1: "700.00", income_occurance_1: "fortnightly" }
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, '<p>Income saved. You will be able to edit it later.</p>')
+    assert_includes(last_response.body, '<form id="add_category">')
+    assert_includes(last_response.body, '<form method="post" action="/expenses" id="save_expenses">')
+    assert_includes(last_response.body, '<a herf="#" class="add_input">+ Expense</a>')
+    assert_includes(last_response.body, '<button type="submit">Save</button>')
+  end
 end
