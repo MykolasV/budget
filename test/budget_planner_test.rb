b12762d6f99ec7be089bbfe88ba59d9f04bac8f7
Mykolas Viningas <m.viningas@gmail.com>
2022-10-31 21:14:55 +0000

    Added tests for saving income and expenses

--- test/budget_planner_test.rb
+++ test/budget_planner_test.rb
@@ -60,15 +60,19 @@ class BudgetTest < Minitest::Test
     assert_includes(last_response.body, '<button type="submit">Save</button>')
   end
 
-  def test_expenses_with_no_income
-    get "/expenses"
+  def test_save_income
+    post "/income", { income_name_1: "salary", income_amount_1: "700.00", income_occurance_1: "fortnightly" }
     assert_equal(302, last_response.status)
-    assert_includes(session[:error_messages], 'Please provide some details about your income.')
-
+    assert_equal("Income saved. You will be able to edit it later.", session[:success_message])
+    
     get last_response["Location"]
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, '<form method="post" action="/income" id="save_income">')
+    assert_includes(last_response.body, '<h2>Expenses</h2>')
+    assert_includes(last_response.body, '<form id="add_category">')
+    assert_includes(last_response.body, '<form method="post" action="/expenses" id="save_expenses">')
+    assert_includes(last_response.body, '<a herf="#" class="add_input">+ Expense</a>')
+    assert_includes(last_response.body, '<button type="submit">Save</button>')
   end
 
   def test_expenses
@@ -82,6 +86,17 @@ class BudgetTest < Minitest::Test
     assert_includes(last_response.body, '<button type="submit">Save</button>')
   end
 
+  def test_expenses_with_no_income
+    get "/expenses"
+    assert_equal(302, last_response.status)
+    assert_includes(session[:error_messages], 'Please provide some details about your income.')
+
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, '<form method="post" action="/income" id="save_income">')
+  end
+
   def test_expenses_with_expenses
     get "/expenses", {}, { 
       "rack.session" => { 
@@ -92,6 +107,30 @@ class BudgetTest < Minitest::Test
 
     assert_equal(302, last_response.status)
     assert_equal('Expenses saved.', session[:success_message])
+
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, '<h1>Budget Planner</h1>')
+    assert_includes(last_response.body, '<h2>Summary</h2>')
+    assert_includes(last_response.body, '<select id="occurance" data-occurance="monthly">')
+    assert_includes(last_response.body, '<option value="monthly" selected>Monthly</option>')
+    assert_includes(last_response.body, '<h3>Income: £<span class="amount" data-monthly-amount="1520.83">1520.83</span></h3>')
+    assert_includes(last_response.body, '<h3>Expenses: £<span class="amount" data-monthly-amount="462.0">462.0</span></h3>')
+    assert_includes(last_response.body, '<h3>Spare Cash: £<span class="amount" data-monthly-amount="1058.83">1058.83</span></h3>')
+  end
+
+  def test_save_expenses
+    post "/expenses",
+    {
+      category_name_1: "essentials", essentials_name_1: "rent", essentials_amount_1: "462.00", essentials_occurance_1: "monthly"
+    },
+    {
+      "rack.session" => { income: { "1" => { name: "salary", amount: "700.00", occurance: "fortnightly" } } }
+    }
+
+    assert_equal(302, last_response.status)
+    assert_equal("Expenses saved.", session[:success_message])
 
     get last_response["Location"]
     assert_equal(200, last_response.status)
